fc25a4b809aebbf246c68e5fac43864b0536c093
aliiasad <alii.asad0519@gmail.com>
2026-03-24 10:56:46 +0500

    DrawMarks and DrawX Added

--- main.cpp
+++ main.cpp
@@ -10,6 +10,8 @@ void handleEvents(RenderWindow&, TicTacToe&);
 void render(RenderWindow&, Font&, TicTacToe&);
 void drawGrid(RenderWindow&);
 void drawStatus(RenderWindow&, Font&, TicTacToe&);
+void drawMarks(RenderWindow&, TicTacToe&);
+void drawX(RenderWindow&, int, int);
 
 int main()  {
     RenderWindow window(VideoMode({600u, 660u}), "TicTacToe");
@@ -41,11 +43,11 @@ void handleEvents(RenderWindow& window, TicTacToe& game) {
         if (event->is<Event :: Closed>())     window.close();
 
         if (const auto* key = event->getIf<Event::KeyPressed>())
-            if (key->code == Keyboard::Key::R)
+            if (key->code == Keyboard :: Key :: R)
                 game.reset();
 
-        if (const auto* click = event->getIf<Event::MouseButtonPressed>()) {
-            if (click->button == Mouse::Button::Left) {
+        if (const auto* click = event->getIf<Event :: MouseButtonPressed>()) {
+            if (click->button == Mouse :: Button :: Left) {
                 float mx = (float)click->position.x;
                 float my = (float)click->position.y;
 
@@ -65,6 +67,7 @@ void render(RenderWindow& window, Font& font, TicTacToe& game) {
     window.clear(Color(30, 30, 30));
     drawStatus(window, font, game);
     drawGrid(window);
+    drawMarks(window, game);
     window.display();
 }
 
@@ -118,4 +121,32 @@ void drawStatus(RenderWindow& window, Font& font, TicTacToe& game)    {
     txt.setPosition({(600.f - b.size.x) * 0.5f - b.position.x,
                      (60.f - b.size.y) * 0.5f - b.position.y});
     window.draw(txt);
+}
+
+void drawMarks(RenderWindow& window, TicTacToe& game)   {
+    for (int row = 0; row < 3; row++) {
+        for (int col = 0; col < 3; col++) {
+            char cell = game.getCell(row, col);
+            if (cell == 'X') drawX(window, row, col);
+        }
+    }
+}
+
+void drawX(RenderWindow& window, int row, int col)   {
+    // left edge with margin   = x + 40
+    // right edge with margin  = x + 200 - 40 = x + 160
+    // top edge with margin    = y + 40
+    // bottom edge with margin = y + 200 - 40 = y + 160
+    float x = col * 200.f;
+    float y = row * 200.f + 60.f;
+
+    Vertex diagonalLine1[2] = {Vertex {Vector2f(x + 40, y + 40), Color :: Green},
+                               Vertex {Vector2f(x + 160, y+ 160), Color :: Green}};
+    window.draw(diagonalLine1, 2, PrimitiveType :: Lines);
+
+    Vertex diagonalLine2[2] = {Vertex {Vector2f(x + 160, y + 40), Color :: Green},
+                               Vertex {Vector2f(x + 40, y + 160), Color :: Green}};
+    window.draw(diagonalLine2, 2, PrimitiveType :: Lines);
+
+    return;
 }
